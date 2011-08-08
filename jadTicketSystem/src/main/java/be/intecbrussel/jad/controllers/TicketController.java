4ce4a5ae62106ee93c41d7cdac08dbf053776b0b
jam-jam <elouaaliti.j@gmail.com>
2011-08-08 15:03:33 +0200

    3w

--- jadTicketSystem/src/main/java/be/intecbrussel/jad/controllers/TicketController.java
+++ jadTicketSystem/src/main/java/be/intecbrussel/jad/controllers/TicketController.java
@@ -1,16 +1,22 @@
-package be.intecbrussel.jad.controller;
+package be.intecbrussel.jad.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+//@RequestMapping(value="tick")
 public class TicketController {
 	
 	@RequestMapping(method=RequestMethod.GET, value="ticket")
 	public String toReg() {
 		System.out.println("in toReg");
-		return "ticketRegistration";
+		return "user";
+	}
+	@RequestMapping(method=RequestMethod.GET, value="newTicket")
+	public String toNewTicket(){
+		
+		return "newticket";
 	}
 	
 }
